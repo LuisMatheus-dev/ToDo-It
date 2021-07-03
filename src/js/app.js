15dcf71fddd07b3a30cb0f799e22ede4ce43d517
LuisMatheus.dev <luismatheusbs.2016@gmail.com>
2021-07-03 18:31:44 -0300

    :rocket: [feat] Sombra Scroll, custom scroll bar

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,61 @@
 const tasks = document.querySelector('.tarefas__lista');
+const sombra = document.querySelector('.sombra');
+
 
 tasks.addEventListener('click', (event) => {
   
   const task = event.target.closest('.tarefas__item');
-  const taskId = task.dataset.tarefa;
-  const checkbox = document.querySelector(`#tarefa-${taskId}`);
-  const riscado = document.querySelector(`.riscado--tarefa-${taskId}`);
-  const taskTitle = task.textContent.trim().split('\n')[4].trim().replace(/ /g,'').length
-  const pxPorCaractere = taskTitle * 9;
-  /**
-   * Quando o evento click ocorre no checkbox, o DOM retornara o estado
-   * do checkbox antes que o arvore seja atualizada com o atributo checked
-   * resultando em nenhuma mudança no checkbox
-   */
-  console.log(checkbox.checked)
-  if(checkbox && ( event.target !== checkbox )) {
-    checkbox.checked = !checkbox.checked;
-    riscado.style.width = checkbox.checked ? `${pxPorCaractere}px` : 0;
+
+  if(task !== null) {
+
+    const taskTitle = task.textContent.trim().split('\n')[4].trim().replace(/ /g,'').length
+    const taskId = task.dataset.tarefa;
+    const checkbox = document.querySelector(`#tarefa-${taskId}`);
+    const riscado = document.querySelector(`.riscado--tarefa-${taskId}`);
+    const pxPorCaractere = taskTitle * 9;
+    
+    /**
+     * Quando o evento click ocorre no checkbox, o DOM retornara o estado
+     * do checkbox antes que o arvore seja atualizada com o atributo checked
+     * resultando em nenhuma mudança no checkbox
+     */
+    const efeitoConcluido = () => {
+      task.style.backgroundColor =  checkbox.checked ? '#9C3D54' : '#FFE6C5'
+      task.classList.toggle('concluido')
+      riscado.style.width = checkbox.checked ? `${pxPorCaractere}px` : 0;
+    }
+
+    if(event.target !== checkbox) {
+      
+      checkbox.checked = !checkbox.checked;
+      efeitoConcluido();
+
+    } 
+
+    if(event.target === checkbox) {
+      efeitoConcluido();
+    }
+
   }
 
 });
+
+let scrollOn = false
+tasks.addEventListener('wheel', () => { 
+  /**
+   * Garante que o efeito seja chamado uma vez 
+   * quando o usuario parar o scroll 
+   */
+  if(!scrollOn) {
+    sombra.classList.add('sombra-expandir')
+    
+    //Função será executada de forma assincrona
+    setTimeout(() => {
+      sombra.classList.remove('sombra-expandir');
+      scrollOn = false;
+    },1000)
+    scrollOn = true;
+  }
+});  
+
 
